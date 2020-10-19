bfe5be7c5e42fec605e8cdb9101955cba5fda2ff
Sarah Lehman <lehmansarahm@gmail.com>
2020-10-19 17:21:53 -0400

    Refactoring for clarity

--- android/ml_kit/src/main/java/edu/temple/mar_security/ml_kit/MainActivity.java
+++ android/ml_kit/src/main/java/edu/temple/mar_security/ml_kit/MainActivity.java
@@ -3,13 +3,6 @@ package edu.temple.mar_security.ml_kit;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.ImageFormat;
-import android.graphics.Matrix;
-import android.graphics.Rect;
-import android.graphics.YuvImage;
-import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -24,27 +17,17 @@ import androidx.core.content.ContextCompat;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity
-        implements ActivityCompat.OnRequestPermissionsResultCallback,
-        FaceAnalyzer.FaceAnalysisListener {
+        implements ActivityCompat.OnRequestPermissionsResultCallback {
 
     public static final String TAG = "MAR_Security_MLKit";
 
     private static final boolean ACCURACY_OVER_SPEED = false;
-    private static final String FILENAME_FORMAT = "yyyyMMdd_HHmmssSSS";
-    private static final SimpleDateFormat SDF = new SimpleDateFormat(FILENAME_FORMAT);
 
     private static final int PERMISSION_REQUESTS = 999;
     private static final String[] PERMISSIONS = new String[] {
@@ -52,6 +35,7 @@ public class MainActivity extends AppCompatActivity
     };
 
     private ExecutorService cameraExecutor;
+    private FaceProcessor processor;
     private PreviewView previewView;
 
     // ----------------------------------------------------------------------------------
@@ -64,6 +48,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         cameraExecutor = Executors.newSingleThreadExecutor();
+        processor = new FaceProcessor(this);
         previewView = findViewById(R.id.viewFinder);
 
         if (allPermissionsGranted()) startCamera();
@@ -94,7 +79,7 @@ public class MainActivity extends AppCompatActivity
 
             ImageAnalysis analysis = new ImageAnalysis.Builder().build();
             analysis.setAnalyzer(cameraExecutor,
-                    new FaceAnalyzer(this, ACCURACY_OVER_SPEED));
+                    new FaceAnalyzer(processor, ACCURACY_OVER_SPEED));
 
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
@@ -104,27 +89,6 @@ public class MainActivity extends AppCompatActivity
                 Log.e(TAG, "Something went wrong while trying to start the camera!", ex);
             }
         }, ContextCompat.getMainExecutor(this));
-    }
-
-    @Override
-    public void facesFound(Image image, int rotation, List<Rect> boundingBoxes) {
-        Log.i(TAG, "Received image with " + boundingBoxes.size()
-                + " bounding boxes and rotation: " + rotation);
-
-        try {
-            for (Rect boundingBox : boundingBoxes) {
-                // extract the faces according to the bounding box coordinates
-                Bitmap bitmap = toBitmap(image, rotation);
-                Bitmap croppedBitmap = crop(bitmap, boundingBox);
-                writeToFile(croppedBitmap);
-
-                // TODO - scale the face snippets to a useful size
-                // TODO - extract RGB matrices
-                // TODO - feed inputs to Kunal's CV modules
-            }
-        } catch (Exception ex) {
-            Log.e(TAG, "Something went wrong while attempting to process detected faces!", ex);
-        }
     }
 
     // ----------------------------------------------------------------------------------
@@ -172,76 +136,4 @@ public class MainActivity extends AppCompatActivity
         return false;
     }
 
-    // ----------------------------------------------------------------------------------
-    //      FILE I/O
-    // ----------------------------------------------------------------------------------
-
-    private Bitmap toBitmap(Image image, int rotation) {
-        Image.Plane[] planes = image.getPlanes();
-        ByteBuffer yBuffer = planes[0].getBuffer();
-        ByteBuffer uBuffer = planes[1].getBuffer();
-        ByteBuffer vBuffer = planes[2].getBuffer();
-
-        int ySize = yBuffer.remaining();
-        int uSize = uBuffer.remaining();
-        int vSize = vBuffer.remaining();
-
-        byte[] nv21 = new byte[ySize + uSize + vSize];
-        //U and V are swapped
-        yBuffer.get(nv21, 0, ySize);
-        vBuffer.get(nv21, ySize, vSize);
-        uBuffer.get(nv21, ySize + vSize, uSize);
-
-        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21,
-                image.getWidth(), image.getHeight(), null);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        yuvImage.compressToJpeg(new Rect(0, 0,
-                yuvImage.getWidth(), yuvImage.getHeight()), 100, out);
-
-        byte[] imageBytes = out.toByteArray();
-        Bitmap original = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
-
-        Matrix rotationMatrix = new Matrix();
-        rotationMatrix.postRotate((float) rotation);
-        Bitmap rotatedBitmap = Bitmap.createBitmap(original, 0, 0,
-                original.getWidth(), original.getHeight(), rotationMatrix, true);
-
-        // return original;
-        return rotatedBitmap;
-    }
-
-    private Bitmap crop(Bitmap original, Rect boundingBox) {
-        Log.i(TAG, "Attempting to crop bitmap with height: " + original.getHeight()
-                + " and width: " + original.getWidth()
-                + " \t\t using bounding box with top: " + boundingBox.top
-                + ", bottom: " + boundingBox.bottom
-                + ", left: " + boundingBox.left
-                + ", right: " + boundingBox.right
-                + ", height: " + boundingBox.height()
-                + ", width: " + boundingBox.width());
-        return Bitmap.createBitmap(original, boundingBox.left, boundingBox.top,
-                boundingBox.width(), boundingBox.height());
-    }
-
-    private File getOutputDirectory() {
-        File outputDir = getExternalFilesDir("");
-        if (!outputDir.exists()) outputDir.mkdir();
-        return outputDir;
-    }
-
-    private void writeToFile(Bitmap image) {
-        String filename = (SDF.format(new Date()) + ".png");
-        File file = new File(getOutputDirectory(), filename);
-
-        try {
-            Log.i(TAG, "Writing image to file: " + file.getAbsolutePath());
-            FileOutputStream fOut = new FileOutputStream(file);
-            image.compress(Bitmap.CompressFormat.PNG, 100, fOut);
-            fOut.flush();
-            fOut.close();
-        } catch (IOException ex) {
-            Log.e(TAG, "Something went wrong while attempting to write image to file: "
-                    + file.getAbsolutePath(), ex);
-        }
-    }
 }
